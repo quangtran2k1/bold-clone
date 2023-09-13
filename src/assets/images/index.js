91d23232dd81c77612b0cdcf95342bfde00de37b
quangtran2k1 <01677092001a@gmail.com>
2023-09-14 01:04:05 +0700

    14/9

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,9 @@
 const images = {
     logo: require('~/assets/images/logo.svg').default,
     appIcon: require('~/assets/images/App-Icon.svg').default,
+    phoneImg: require('~/assets/images/iPhone-11-Pro-Space-Grey-1.png'),
+    charListImg: require('~/assets/images/ChatList-White-p-500.jpeg'),
+    lockScreenImg: require('~/assets/images/LockScreen-Light.jpg'),
 };
 
 export default images;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,22 +8,22 @@ const cx = classNames.bind(styles);
 
 export default function Home() {
     return (
-        <div className={cx('content__wrapper')}>
-            <div cx={cx('content__eclipse__holder')}></div>
-            <div cx={cx('content__main__container')}>
-                <div className={cx('content__inner')}>
-                    <div className={cx('content__inner__text')}>
-                        <div className={cx('content__inner__icon')}>
+        <div className={cx('wrapper')}>
+            <div cx={cx('eclipse__holder')}></div>
+            <div cx={cx('main__container')}>
+                <div className={cx('inner')}>
+                    <div className={cx('inner__text')}>
+                        <div className={cx('inner__icon')}>
                             <img src={images.appIcon} alt="app icon" />
                         </div>
-                        <h1 className={cx('content__main__heading')}>Manage it all, in this all new system.</h1>
-                        <div className={cx('content__grid')}>
-                            <p className={cx('content__inner_desc')}>
+                        <h1 className={cx('main__heading')}>Manage it all, in this all new system.</h1>
+                        <div className={cx('grid')}>
+                            <p className={cx('inner_desc')}>
                                 Bold keeps your team’s work on-brand, on message, and on time. Innovative features make
                                 creating and sharing your work feel effortless.
                             </p>
                         </div>
-                        <div className={cx('content__download')}>
+                        <div className={cx('download')}>
                             <div className={cx('download__ios')}>
                                 <a
                                     href="http://apple.com"
@@ -32,7 +32,7 @@ export default function Home() {
                                     className={cx('download__btn', 'button')}
                                 >
                                     <AppleFilled />
-                                    Download for IOS
+                                    Download for iOS
                                 </a>
                             </div>
                             <div className={cx('download__android', 'last')}>
@@ -45,6 +45,46 @@ export default function Home() {
                                     <AndroidFilled />
                                     Download for Android
                                 </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('inner')}>
+                <div className={cx('text__sticky')}>
+                    <div className={cx('text__holder')}>
+                        <div className={cx('text__holder__1')}>
+                            <h1 className={cx('text', '_1')}>Meet</h1>
+                        </div>
+                        <div className={cx('text__line')}></div>
+                        <div className={cx('text__holder _2')}>
+                            <h1 className={cx('text', '_2')}>Bold</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={cx('product__demo')}>
+                    <div className={cx('product__sticky')}>
+                        <div className={cx('product__phone__image__wrapper')}>
+                            <div className={cx('product__container')}>
+                                <div className={cx('product__phone__image__holder')}>
+                                    <img
+                                        src={images.phoneImg}
+                                        alt="PhoneImage"
+                                        className={cx('product__phone__image')}
+                                    />
+                                    <div className={cx('product__phone__content')}>
+                                        <img
+                                            src={images.charListImg}
+                                            alt="char list img"
+                                            className={cx('product__phone__demo__image')}
+                                        />
+                                        <img
+                                            src={images.lockScreenImg}
+                                            alt="lock screen img"
+                                            className={cx('iphone__lock__screen__image')}
+                                        />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
